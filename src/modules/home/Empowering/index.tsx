8c8b81bf3731cebcaafecb477a9d3aed91898b07
Mohd Aamil <mohd@omegaorion.com>
2026-02-06 16:52:26 +0530

    fix: deployment error

--- src/modules/home/Empowering/index.tsx
+++ src/modules/home/Empowering/index.tsx
@@ -7,10 +7,10 @@ import Typography from '@/components/Typography';
 import { cn } from '@/lib';
 import Image from 'next/image';
 
-import Img1 from '/public/images/AboutImages/about-image-14.jpg';
-import Img2 from '/public/images/AboutImages/about-image-13.jpg';
+import Img1 from '/public/images/AboutImages/about-image-14.JPG';
+import Img2 from '/public/images/AboutImages/about-image-13.JPG';
 import Img3 from '/public/images/AboutImages/about-image-12.jpg';
-import Img4 from '/public/images/AboutImages/about-image-11.jpg';
+import Img4 from '/public/images/AboutImages/about-image-11.JPG';
 import Img5 from '/public/images/AboutImages/about-image-8.png';
 import Img6 from '/public/images/AboutImages/about-image-9.png';
 
